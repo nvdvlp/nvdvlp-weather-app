13cfa8e4cf3deeb1d8f71058db885737ab58ce04
Santiago Nieves <137436428+nvdvlp@users.noreply.github.com>
2024-08-15 12:44:05 -0500

    weather card's and city, country parameters done

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,15 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { WeatherService } from './weather-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule, HttpClientModule],
+  providers: [WeatherService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'weather-app';
+
+export class AppComponent implements OnInit {
+  weatherData: any;
+  idWeather: number = 0;
+  cityName: string = '';
+  countryName: string ='';
+  date: Date = new Date();
+  daysOfTheWeek:string[] = ['Sunday','Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday'];
+  dayName:string = this.daysOfTheWeek[this.date.getDay()].toUpperCase();
+  hours:number = this.date.getHours();
+  minutes:number = this.date.getMinutes();
+  zoneTime: string = '';
+  degressCelsius: number = 0;
+  Floordegress: number = Math.floor(this.degressCelsius);
+
+  states = {
+    Drizzle: false,
+    Rain: false,
+    Thunderstorm: false, 
+    Snow: false,
+    Atmosphere: false,
+    Clouds: false,
+    Clear:false
+  };
+  
+  constructor(private weatherService: WeatherService) {}
+  
+  ngOnInit(): void {
+    this.weatherService.getWeather().subscribe(
+      (data) => {
+        this.weatherData = data;
+        console.log(this.weatherData = data);
+        this.idWeather = data.weather[0].id;
+        this.updateStates();
+        this.cityName = data.name;
+        this.countryName = data.sys.country;
+        this.setZoneTime();
+        console.log(this.cityName = data.name);
+        console.log(this.countryName = data.sys.country);
+        console.log(this.dayName);
+        this.Floordegress = this.degressCelsius = data.main.temp
+        console.log( this.Floordegress);
+      },
+      (error: any) => { 
+        console.log('Error fetching weather data:', error);
+      }
+    );
+  }
+  
+  updateStates(): void {
+    if (this.idWeather >= 200 && this.idWeather < 232) {
+      this.states.Thunderstorm = true;
+    }
+    if (this.idWeather >= 300 && this.idWeather < 321) {
+      this.states.Drizzle = true;
+    }
+    if (this.idWeather >= 500 && this.idWeather < 531) {
+      this.states.Rain = true;
+    }
+    if (this.idWeather >= 600 && this.idWeather < 622) {
+      this.states.Snow = true;
+    }
+    if (this.idWeather >= 701 && this.idWeather < 781  ) {
+      this.states.Atmosphere = true;
+    }
+    if (this.idWeather >= 701 && this.idWeather < 781  ) {
+      this.states.Atmosphere = true;
+    }
+    if (this.idWeather == 800 ) {
+      this.states.Clear = true;
+    }
+    if (this.idWeather >= 801 && this.idWeather < 804) {
+      this.states.Clouds = true;
+    }
+  }
+
+  setZoneTime(){
+    if(this.hours >= 0 && this.hours > 12){
+      this.zoneTime = 'AM'
+    }else{
+      this.zoneTime = 'PM'
+    }
+    
+  }
+  
 }
---
+++ weather-app/src/app/weather-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=New+York&appid=14f1488c19996272466d2776e8c52221&units=metric';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
